667f8d54f35d510b6c34602d04e9bd719ea2a9b0
Sergey <Shestakowsy@gmail.com>
2024-04-30 04:24:32 +0300

    Добавлен проект BLL

---
+++ WebService.BLL/EntityService.cs
@@ -0,0 +1,45 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebService.BLL
+{
+    public class EntityService : IEntityService<Entity>
+    {
+        private readonly IApplicationDbContext _context;
+        public EntityService(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Entity> FindAsync(Guid id)
+        {
+            return await _context.Entities.FindAsync(id);
+        }
+
+        public async Task<Entity> AddAsync(Entity entity)
+        {
+            _context.Entities.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<Entity> UpdateAsync(Entity entity)
+        {
+            var existingEntity = await _context.Entities.FindAsync(entity.Id);
+            if (existingEntity != null)
+            {
+                existingEntity.Amount = entity.Amount;
+                existingEntity.OperationDate = entity.OperationDate;
+                await _context.SaveChangesAsync();
+            }
+            return existingEntity;
+        }
+
+        public Task<Entity> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
+   
---
+++ WebService.BLL/IEntityService.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+
+namespace WebService.BLL
+{
+    public interface IEntityService<T> where T : class , IEntity
+    {
+         Task<T> FindAsync(Guid id);
+
+        Task<T> AddAsync(Entity entity);
+
+        Task<T> UpdateAsync(Entity entity);
+
+        Task<T> DeleteAsync(Guid id);
+    }
+}
