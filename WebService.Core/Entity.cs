56a827a02806002566ffad3364423117fadd2eb2
Sergey <Shestakowsy@gmail.com>
2024-04-30 04:18:17 +0300

    Добавлен проект Core

---
+++ WebService.Core/Entity.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain
+{
+    public class Entity : IEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime OperationDate { get; set; }
+        public decimal Amount { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+            OperationDate = DateTime.Now;
+        }
+    }
+}
---
+++ WebService.Core/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain
+{
+    public interface IEntity
+    {
+        Guid Id { get; set; }
+    }
+}
